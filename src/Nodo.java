8a12fe21def8f1e818028e90ab9b9452a7935b5b
Mateo Diaz <maadiazmu@unal.edu.co>
2021-03-21 16:02:03 -0500

    add_node

---
+++ src/Nodo.java
@@ -0,0 +1,27 @@
+public class Nodo {
+
+    private Nodo siguiente;
+
+    public void Nodo(){
+        this.valor = 0;
+        this.siguiente = null;
+    }
+    
+
+    
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }   
+}
